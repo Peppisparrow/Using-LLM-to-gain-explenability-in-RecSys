39429d427fb2685f672127d2d04e57e7fb51b923
Peppisparrow <giuseppe1.vitello@mail.polimi.it>
2025-07-13 15:19:19 +0200

    fix optuna 2 tower with embeddings

--- Prototype/optuna/optuna_two_tower_product_prototype.py
+++ Prototype/optuna/optuna_two_tower_product_prototype.py
@@ -5,22 +5,33 @@ import numpy as np
 import optuna
 import pandas as pd
 import torch
+from argparse import ArgumentParser
 # Defining Recommender
 from RecSysFramework.Recommenders.Neural.TwoTowerRecommenderEmbeddingsPrototype import TwoTowerRecProductNorm
-from Prototype.data_manager import DataManger
+from Prototype.data_manager_peppe import DataManger
 from Prototype.utils.optuna_utils import SaveResults
 from RecSysFramework.Evaluation.Evaluator import EvaluatorHoldout
 # ---------- CONSTANTS ----------
 METRIC = 'MAP_MIN_DEN'
 METRIC_K = 10
 BASE_OPTUNA_FOLDER = Path("Prototype/optuna/")
-STUDY_NAME = "2Tower_product_norm_prototype"
-DATA_PATH = Path('Prototype/Dataset/steam/filtering_no_desc_giappo_corean_k10')
-USER_EMBEDDING_PATH = Path('Prototype/Dataset/steam/filtering_no_desc_giappo_corean_k10/user_embeddings_compressed_t5.npz')
-ITEM_EMBEDDING_PATH = Path('Prototype/Dataset/steam/filtering_no_desc_giappo_corean_k10/game_embeddings_t5.npz')
+# STUDY_NAME = "2Tower_product_norm_prototype"
+# DATA_PATH = Path('Prototype/Dataset/steam/filtering_no_desc_giappo_corean_k10')
+# USER_EMBEDDING_PATH = Path('Prototype/Dataset/steam/filtering_no_desc_giappo_corean_k10/user_embeddings_compressed_t5.npz')
+# ITEM_EMBEDDING_PATH = Path('Prototype/Dataset/steam/filtering_no_desc_giappo_corean_k10/game_embeddings_t5.npz')
 # ---------- /CONSTANTS ----------
 
 def objective_function(trial, URM_train, URM_test, item_embeddings=None, user_embeddings=None):
+
+    if user_embeddings is not None:
+        print("Using user embeddings from the data manager.")
+        user_embeddings_dim = user_embeddings.shape[1]
+    else:
+        user_embeddings_dim = None
+    if item_embeddings is not None:
+        item_embeddings_dim = item_embeddings.shape[1]
+    else:
+        item_embeddings_dim = None
 
     epochs = trial.suggest_int("epochs", 5, 10)
     batch_size = trial.suggest_int("batch_size", 512, 4096)
@@ -34,7 +45,7 @@ def objective_function(trial, URM_train, URM_test, item_embeddings=None, user_em
     layers = np.linspace(input, output, n_layers, dtype=np.int16)
     layers = layers.astype(int)
     print(f"Current layers: {layers}")
-    recommender = TwoTowerRecProductNorm(URM_train, URM_train.shape[0], URM_train.shape[1], user_embeddings_dim=user_embeddings.shape[1], item_embeddings_dim=item_embeddings.shape[1], layers=layers, verbose=True, first_dim_layer=32)
+    recommender = TwoTowerRecProductNorm(URM_train, URM_train.shape[0], URM_train.shape[1], user_embeddings_dim=user_embeddings_dim, item_embeddings_dim=item_embeddings_dim, layers=layers, verbose=True, first_dim_layer=32)
     print(f"Current parameters: epochs={epochs}, batch_size={batch_size}, learning_rate={learning_rate}, weight_decay={weight_decay}, layers={layers}")
     optimizer = torch.optim.AdamW(params=recommender.parameters(), lr=learning_rate, weight_decay=weight_decay, fused=True)
     print("Optimizer initialized.")
@@ -52,8 +63,15 @@ def main():
     data_manager = DataManger(data_path=DATA_PATH, user_embedding_path=USER_EMBEDDING_PATH, item_embeddings_path=ITEM_EMBEDDING_PATH)
     URM_train = data_manager.get_URM_train()
     URM_test = data_manager.get_URM_test()
-    item_embeddings = data_manager.get_item_embeddings()
-    user_embeddings = data_manager.get_user_embeddings()
+    if USER_EMBEDDING_PATH is not None:
+        print("Loading user embeddings from path:", USER_EMBEDDING_PATH)
+        user_embeddings = data_manager.get_user_embeddings()
+    else:
+        user_embeddings = None
+    if ITEM_EMBEDDING_PATH is not None:
+        item_embeddings = data_manager.get_item_embeddings()
+    else:
+        item_embeddings = None
     objective_function_with_data = partial(
         objective_function,
         URM_train=URM_train,
@@ -69,6 +87,27 @@ def main():
     optuna_study.optimize(objective_function_with_data,
                         callbacks=[save_results],
                         n_trials = 100)
-
+    
 if __name__ == "__main__":
+    parser = ArgumentParser(description="Run Optuna study for IALS with fixed alpha")
+    parser.add_argument("--study_name", type=str, help="Name of the Optuna study")
+    parser.add_argument("--data_path", type=str, help="Path to the dataset with train and test csv files")
+    parser.add_argument("--user_embedding_path", type=str, help="Path to the user embeddings file", default=None)
+    parser.add_argument("--item_embedding_path", type=str, help="Path to the item embeddings file", default=None)
+    args = parser.parse_args()
+    
+    
+    STUDY_NAME = args.study_name
+    DATA_PATH = Path(args.data_path)
+    if args.user_embedding_path is not None:
+        USER_EMBEDDING_PATH = Path(args.user_embedding_path)
+    else:
+        USER_EMBEDDING_PATH = None
+    if args.item_embedding_path is not None:
+        ITEM_EMBEDDING_PATH = Path(args.item_embedding_path)
+    else:
+        ITEM_EMBEDDING_PATH = None
+    
+    print(f"Running study: {STUDY_NAME} with data path: {DATA_PATH} and user embedding path: {USER_EMBEDDING_PATH}")
+    
     main()
